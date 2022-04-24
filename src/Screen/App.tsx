e7d39b5c0adc8e67711f133a0e3ae15948310795
Felipe Augusto <felipeaugusto@Elises-MacBook-Air.local>
2022-04-24 15:42:11 -0300

    feat: load more courses

--- src/Screen/App.tsx
+++ src/Screen/App.tsx
@@ -3,21 +3,21 @@ import './App.css'
 import Navigation from '../Components/Navigation/Navigation'
 import CourseCard from '../Components/CourseCard/CourseCard'
 import getAll from '../Data/api'
-import Courses from '../Data/model'
 function App() {
 
   const [courseArray, setList] = useState([])
+  const [limit, setLimit] = useState(8)
 
 const getAllCourses = () => { 
      getAll().then((res: any) => { 
     console.log(res.data)
      setList(res.data)
   })
-
 }
-useEffect(() => { 
-  console.log("updated", courseArray)
-  }, [courseArray])
+const showAll = () => {
+  const newLimit = limit + 4
+  setLimit(newLimit)
+}
 
  useEffect(() => { 
   getAllCourses()
@@ -29,12 +29,13 @@ useEffect(() => {
     <div className="App">
       <Navigation />
       <div className='course-list'>
-      
-        { courseArray?.map((card: any, i: any) => {
-      return <CourseCard key={i} course={card} />;
-    })
-    }
+        { 
+        courseArray?.slice(0, limit).map((card: any, i: any) => {
+        return <CourseCard key={i} course={card} />;
+          })
+         }
       </div>
+      <button className='button-showall medium dark' onClick={showAll}>Ver mais</button>
     </div>
   )
 }
